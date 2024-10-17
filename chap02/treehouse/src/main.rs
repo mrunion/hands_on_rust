fd209ef53077438e984e217145d73e00c88a572f
Matt Runion <mrunion@eastman.com>
2024-10-17 15:23:59 -0400

    Checking a list of names in an array to see if access to the Treehouse is allowed

--- chap02/treehouse/src/main.rs
+++ chap02/treehouse/src/main.rs
@@ -19,5 +19,17 @@ fn main() {
 
     let name = what_is_your_name();
 
-    println!("Hello, {name}");
+    let visitor_list = ["bert", "matt", "steve", "alice"];
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {name}");
+    } else {
+        println!("Sorry, you aren't on the list");
+    }
 }
