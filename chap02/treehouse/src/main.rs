bd9fa2f51fb0c82d6af3eb4e8263834fdd12a9e6
Matt Runion <mrunion@eastman.com>
2024-10-17 15:13:43 -0400

    Moved name input to a function

--- chap02/treehouse/src/main.rs
+++ chap02/treehouse/src/main.rs
@@ -2,13 +2,20 @@
 
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what is your name?");
-
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
+
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
 
-    println!("Hello, {your_name}");
+    your_name
+}
+
+fn main() {
+    println!("Hello, what is your name?");
+
+    let name = what_is_your_name();
+
+    println!("Hello, {name}");
 }
